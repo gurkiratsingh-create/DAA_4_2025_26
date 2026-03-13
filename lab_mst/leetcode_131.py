36188a59f7055e72bf63e6f4f5de02b1735df4bd
Gurkirat Singh Bhangoo <gurkiratsingh.05.54@gmail.com>
2026-03-13 11:00:06 +0530

    Initializing labmst code

---
+++ lab_mst/leetcode_131.py
@@ -0,0 +1,17 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        path=[]
+        result=[]
+        def backtrack(start):
+            if start==len(s):
+                result.append(path[:])
+                return
+            for end in range(start,len(s)):
+                sub=s[start:end+1]
+                if sub==sub[::-1]:
+                    path.append(sub)
+                    backtrack(end+1)
+                    path.pop()
+        backtrack(0)
+        return result
+        
